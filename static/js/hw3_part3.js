bc79ffcae025ec7c453590f4cf623d27dea97721
Oxana Howard <ovtyutcheva@gmail.com>
2020-02-23 09:33:08 -0800

    Added hw3_part3.js

---
+++ static/js/hw3_part3.js
@@ -0,0 +1,43 @@
+function celsiusToFahrenheit() {
+  var number, fahrenheit;
+
+  number = document.getElementById("number").value;
+
+
+  number = parseFloat(number);
+
+
+  fahrenheit = Math.trunc((9/5 * number) + 32);
+
+  if (Number.isNaN(number)) {
+
+    document.forms["myform"].elements["result"].value = ("You should enter a number!\nTo try again click on RESET");
+
+  } else {
+    document.forms["myform"].elements["result"].value = ("It is " + fahrenheit + " degree Fahrenheit");
+  }
+}
+
+
+
+function fahrenheitToCelsius() {
+  var number, celsius;
+
+  number = document.getElementById("number").value;
+
+
+  number = parseFloat(number);
+
+  celsius = Math.trunc(5/9 * (number - 32));
+
+  if (Number.isNaN(number)) {
+
+    document.forms["myform"].elements["result"].value = ("You should enter a number!\nTo try again click on RESET");
+
+  } else {
+    document.forms["myform"].elements["result"].value = ("It is " + celsius + " degree Celsius");
+
+  }
+}
+
+

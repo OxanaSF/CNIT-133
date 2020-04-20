860ee0c04500839d144556c43f6246a75c9f1587
Oxana Howard <ovtyutcheva@gmail.com>
2020-04-19 20:19:26 -0700

    modified html/css files

--- static/js/hw4_extra_credit.js
+++ static/js/hw4_extra_credit.js
@@ -1,19 +1,23 @@
+var number;
+
 function getSquare() {
-  var number;
 
   number = document.getElementById("number").value;
   n = parseInt(number);
 
   if (Number.isNaN(n) || n > 10 || n < 2) {
 
+
     document.forms["myform"].elements["result2"].value = ("You should enter a digit between 2 and 10 inclusive!\nTo try again click on RESET");
 
   } else {
- 
+  
+  document.forms["myform"].elements["result1"].value = (""); 
 
   for (var i = 0; i < n; i++) {
-    
+     
       for (var j = 0; j < n; j++) {
+
   
       document.forms["myform"].elements["result1"].value += ("*");
   }
